a0363727a2a9960a7d8b9504975af4ba67c0b91c
ddsnowboard <ddsnowboard@gmail.com>
2014-07-24 10:14:27 -0500

    Finished DB 172
    I just made it all a list of lines, it was easier.

--- DP 172/main.py
+++ DP 172/main.py
@@ -5,10 +5,15 @@ for i, j in enumerate(f):
 	f[i] = j.decode('UTF-8').replace(' ','')
 letters = {}
 for i in range(26):
-	letters[f[8*i].replace('\n', '')] = ''.join(f[(8*i)+1:8*(i+1)]).replace(' ', '')
-
+	letters[f[8*i].replace('\n', '')] = f[(8*i)+1:8*(i+1)]
+for i, j in letters.items():
+	for k, p in enumerate(j):
+		j[k] = p.replace(" ", '').replace('\n','').replace('0', ' ')
 inp = input("What do you want to write? ")
 with open('output.pbm', 'w') as o:
-	o.write("P1 \n%d 6" % len(inp) * 5)
-	for i in inp:
-		o.write(letters[i.upper()])
+	o.write("P1 \n{0} {1}\n".format(len(inp)*5, 6))
+	for i in range(7):
+		for j in inp:
+			o.write(letters[j.upper()][i])
+			o.write(' ')
+		o.write('\n')
