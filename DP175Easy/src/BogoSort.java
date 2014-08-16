2b380ff1eb9926757a4c78fbe18f6b20ffd2a465
ddsnowboard <ddsnowboard@gmail.com>
2014-08-16 17:03:28 -0500

    Fixed BogoSort.
    I shouldn't have seeded the Random object. Oops. Anyway, then I make it
    average the output of running it 100,000 times. It's about 60 times. So
    that's cool. Now, to post!

--- DP175Easy/src/BogoSort.java
+++ DP175Easy/src/BogoSort.java
@@ -5,19 +5,31 @@ import java.util.Random;
 public class BogoSort {
 
 	public static void main(String[] args) {
-		int times = 0;
-		String mainString = args[1];
-		String changableString = args[0];
-		while(!mainString.equals(changableString))
+		ArrayList<Integer> times = new ArrayList<Integer>();
+		for(int i=0; i<100000;i++)
 		{
-			changableString = scramble(changableString);
-			times++;
-//			System.out.println(changableString);
+			times.add(bogoSort(args[0], args[1]));
 		}
-		System.out.printf("%d iterations", times);
-		System.out.println("Hi");
+		float avg = 0;
+		long tot = 0;
+		for(int d : times)
+		{
+			tot+=d;
+		}
+		avg = 1.0f * tot/times.size();
+		System.out.printf("Average after 100000 times: %f", avg);
+		
 	}
-
+	public static int bogoSort(String shuffled, String main)
+	{
+		int times = 0;
+		while(!main.equals(shuffled))
+		{
+			shuffled = scramble(shuffled);
+			times++;
+		}
+		return times;
+	}
 	public static String scramble(String str) {
 		ArrayList<Character> in = new ArrayList<Character>();
 		for(int i = 0; i<str.length();i++)
@@ -25,12 +37,12 @@ public class BogoSort {
 			in.add(str.charAt(i));
 		}
 		ArrayList<Character> out = new ArrayList<Character>();
-		Random rand = new Random(82297);
+		Random rand = new Random();
 		String ret = "";
 		int pick = 0;
 		while(!in.isEmpty())
 		{	
-			pick = rand.nextInt(in.size()+1);
+			pick = rand.nextInt(in.size());
 			out.add(in.remove(pick));
 			in.trimToSize();
 		}
