d3c39cc43cac4b3785a6ed908d6bea0faff25ffe
ddsnowboard <ddsnowboard@gmail.com>
2014-09-08 21:14:37 -0500

    Started on DP178Easy.
    I have one method done. I just need to do... 4 more. And add output, which might
    entail some 2D rendering. So that would be interesting, if time-consuming.

---
+++ DP178Easy/src/OrderedPair.java
@@ -0,0 +1,61 @@
+
+public class OrderedPair {
+
+	private float x;
+	private float y;
+
+	public OrderedPair(float x, float y)
+	{
+		this.x = x;
+		this.y= y;
+	}
+	public OrderedPair(String pair)
+	{
+		pair = pair.replace("(", "");
+		pair = pair.replace(")", "");
+		pair = pair.replace(" ","");
+		String[] coords = pair.split(",");
+		this.x = Float.valueOf(coords[0]);
+		this.y = Float.valueOf(coords[1]);
+	}
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+	public void translate(String s) {
+		this.translate(Float.valueOf(s.split(",")[0]), Float.valueOf(s.split(",")[1]));
+		
+	}
+	public void translate(float x, float y)
+	{
+		this.x += x;
+		this.y += y;
+	}
+	public void rotate(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void scale(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void reflect(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void print()
+	{
+		System.out.print("("+this.x+","+this.y+")");
+	}
+}
---
+++ DP178Easy/src/Transformations.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//Spec:
+//	translate(A, B)     - translate by (A, B) [DONE]
+//	rotate(A, B, C)     - rotate around (A, B) by angle C (in radians) clockwise
+//	scale(A, B, C)      - scale relative to (A, B) with scale-factor C
+//	reflect(axis)       - reflect over the given axis
+//	finish()            - end input and print the modified location
+//Also, make a pair of functions for each function, one that takes two floats, and one
+//that takes a string, parses it, and sends it to the first. Except on reflect and finish, 
+//you don't need it for those. 
+
+
+public class Transformations {
+
+	public static void main(String[] args) throws IOException {
+		String input = readInput();
+		ArrayList<String> instructions = new ArrayList<String>(Arrays.asList(input.split("\n")));
+		OrderedPair point = new OrderedPair(instructions.get(0));
+		instructions.remove(0);
+		String current = new String();
+		for(String s : instructions)
+		{
+			current = s;
+			if(current.contains("translate"))
+			{
+				current = clean(current, "translate");
+				point.translate(current);
+			}
+			else if(current.contains("rotate"))
+			{
+				current = clean(current, "rotate");
+				point.rotate(current);
+			}
+			else if(current.contains("scale"))
+			{
+				current = clean(current, "scale");
+				point.scale(current);
+			}
+			else if(current.contains("reflect"))
+			{
+				current = clean(current, "reflect");
+				point.reflect(current);
+			}
+		}
+
+	}
+
+	private static String readInput() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+		String end = "";
+		String newest = reader.readLine();
+		try
+		{
+			while(newest != null)
+			{
+				end+=newest;
+				end+="\n";
+				newest = reader.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally 
+		{
+			reader.close();
+		}
+		return end;
+	}
+
+	private static String clean(String toClean, String s)
+	{
+		toClean = toClean.replace(s, "");
+		toClean = toClean.replace("(", "");
+		toClean = toClean.replace(")", "");
+		toClean = toClean.replace(" ", "");
+		return toClean;
+	}
+}
