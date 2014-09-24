94500bc2d2c1ee5c06f4e2bf05353c2eed0f2757
ddsnowboard <ddsnowboard@gmail.com>
2014-09-24 11:41:11 -0500

    Kept moving. Look at diff, I don't have time to write.

--- DP181Easy/eqSolver.py
+++ DP181Easy/eqSolver.py
@@ -1,25 +1,26 @@
 import re
 class Equation:
-	def __init__(self, eq):
-		this.eq = re.subn(r"^y=|=y$", '', eq)[0]
-		this.eq = this.eq.replace('^', '**')
-		this.coefficients = defaultdict(float)
-		# Do something to this line, maybe a list comprehension using an outputted match object or something, 
+	def __init__(self, eq):					# y=2x^2-3x+5
+		self.coefficients = defaultdict(float)
+		self.eq = re.subn(r"^y=|=y$", '', eq)[0]			# 2x^2-3x+5
+		self.eq = self.eq.replace('^', '**').replace("+", " +").replace("-", ' -')  # 2x**2 -3x +5
+		# Do something to self line, maybe a list comprehension using an outputted match object or something, 
 		# that will append the sign onto the term with it, so I can keep those. Because those are good. 
-		this.terms = re.compile(r"\+|-").split(this.eq)
-		# Use re.search() with "\+|-\d+[A-Za-z]\+|-", then use the returned match object things to split up the 
+		self.terms = self.eq.split(" ")			# "2x**2", "-3x", "+5"
+		# Use re.search() with "[\+-]\d+[A-Za-z]", then use the returned match object things to split up the 
 		# string, including the first character, because that'll be the sign, and that's good. But you'll have to
 		# change the next few lines, ~14-20, to include the signs. But that's fine. Make sure you don't include 
 		# the next sign though, that's no good. 
+		self.terms = [i for i in self.terms if i != ''] # Just to make sure there is nothing problematic in it. 
 		for i in terms:
 			if not re.compile(r"[A-Za-z]").search(i):
-				this.coefficients[0] += float(i)
-			elif re.compile(r"\d?[A-Za-z][+-$]").search(i):
-				this.terms[1]+=float(re.compile(r"[A-Za-z]|\+|-").subn('',i)[0])
-			elif re.compile(r"-{1}\d+[A-Za-z]\*\*\d+").match(i):
-				this.coefficients[i[i.index("**")+2:]] += float(i[:re.compile("[A-Za-z]").search(i).span[1]])
+				self.coefficients[0] += float(i)
+			elif re.compile(r"[\+-]{0-1}\d{0-1}[A-Za-z]").search(i):
+				self.terms[1]+=float(re.compile(r"[A-Za-z]").subn('',i)[0])
+			elif re.compile(r"[\+-]{0-1}\d+[A-Za-z]\*\*\d+").match(i):
+				self.coefficients[i[i.index("**")+2:]] += float(i[:re.compile("[A-Za-z]").search(i).span[1]])
 	def evaluate(x):
 		end = 0
-		for i, j in this.coefficients:
+		for i, j in self.coefficients:
 			end+=j*x**i
 		return end
