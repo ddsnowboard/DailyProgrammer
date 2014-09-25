0cba276da6541981dd1db12d6f1a7092f623c23a
ddsnowboard <ddsnowboard@gmail.com>
2014-09-24 20:23:57 -0500

    Added input.txt and finished DP181Easy.
    I just needed to do the intersection method and the file reading
    input stuff.

--- DP181Easy/eqSolver.py
+++ DP181Easy/eqSolver.py
@@ -1,27 +1,53 @@
 import re
 from collections import defaultdict
 class Equation:
-	def __init__(self, eq):					# y=2x^2-3x+5
+	def __init__(self, eq):	# y=2x^2-3x+5
 		self.coefficients = defaultdict(float)
-		self.eq = re.subn(r"^y=|=y$", '', eq)[0]			# 2x^2-3x+5
+		self.eq = re.subn(r"^y=|=y$", '', eq)[0]   # 2x^2-3x+5
 		self.eq = self.eq.replace('^', '**').replace("+", " +").replace("-", ' -')  # 2x**2 -3x +5
-		# Do something to self line, maybe a list comprehension using an outputted match object or something, 
-		# that will append the sign onto the term with it, so I can keep those. Because those are good. 
-		self.terms = self.eq.split(" ")			# "2x**2", "-3x", "+5"
-		# Use re.search() with "[\+-]\d+[A-Za-z]", then use the returned match object things to split up the 
-		# string, including the first character, because that'll be the sign, and that's good. But you'll have to
-		# change the next few lines, ~14-20, to include the signs. But that's fine. Make sure you don't include 
-		# the next sign though, that's no good. 
-		self.terms = [i for i in self.terms if i != ''] # Just to make sure there is nothing problematic in it. 
+		self.terms = self.eq.split(" ")	 # "2x**2", "-3x", "+5"
+		self.terms = [i for i in self.terms if i != '']
 		for i in self.terms:
 			if not re.compile(r"[A-Za-z]").search(i):
 				self.coefficients[0] += float(i)  # "+5"
-			elif re.compile(r"[\+-]?[\d\.]+[A-Za-z]").search(i):
+			elif re.compile(r"[\+-]?[\d\.]+[A-Za-z]$").search(i):
 				self.coefficients[1]+=float(re.compile(r"[A-Za-z]").subn('',i)[0])  	#"-3" 
 			elif re.compile(r"[\+-]?[\d\.]+[A-Za-z]\*\*\d+").match(i):
-				self.coefficients[i[i.index("**")+2:]] += float(i[:re.compile("[A-Za-z]").search(i).span[1]]) # '2'
+				self.coefficients[i[i.index("**")+2:]] += float(i[:re.compile("[A-Za-z]").search(i).span()[1]-1]) # '2'
+		self.degree = len(self.coefficients)-1
 	def evaluate(self, x):
 		end = 0
 		for i, j in self.coefficients.items():
 			end+=j*x**i
 		return end
+	def intersect(self, other):
+		if not type(other) == type(Equation("2x^2-4x+5")):
+			raise Exception("You seem to have made a stupid; this is supposed to take another equation and find the intersection")
+			return
+		# Left will be variables; right will be constants. 
+		# Left starts as self, right starts as other. 
+		left = defaultdict(float)
+		right = 0
+		for i, j in self.coefficients.items():
+			if i == 0:
+				right-=j
+			else:
+				left[i]+=j
+		for i, j in other.coefficients.items():
+			if i == 0:
+				right+=j
+			else:
+				left[i]-=j
+		if self.degree == 0 and other.degree == 0:
+			return right == 0
+		elif self.degree<=1 and other.degree<=1:
+			return (right/left[1], self.evaluate(right/left[1]))
+		elif self.degree == 2 or other.degree == 2:
+			return (((-1*left[1]+math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]), self.evaluate((-1*left[1]+math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]))), ((-1*left[1]-math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]), self.evaluate((-1*left[1]-math.sqrt(left[1]**2-4*(left[2])*(-1*right)))/(2*left[2]))))
+		else:
+			raise Error("I really can't get an accurate intersection with just this data.")
+with open("input.txt", 'r') as f:
+	a = Equation(f.readline())
+	b = Equation(f.readline())
+print(a.intersect(b))
+input("Press any key...")
