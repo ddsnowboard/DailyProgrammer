3314264761b1d57568aeabc431c82c852f17106d
ddsnowboard <ddsnowboard@gmail.com>
2014-10-07 20:01:12 -0500

    Formatted code
    This netbeans thing might not be so bad.

--- DP183Easy/src/SymanticVersionSort.java
+++ DP183Easy/src/SymanticVersionSort.java
@@ -6,50 +6,56 @@ import java.util.Scanner;
 
 public class SymanticVersionSort {
 
-	public static void main(String[] args) {
-		File f = new File("input.txt");
-		try {
-			Scanner i = new Scanner(f);
-			i.nextInt();		// I didn't really need to use the number, but I needed to get past it. 
-			ArrayList<Version> versions = new ArrayList<>();
-			String temp;
-			while (i.hasNext()) {
-				temp = i.nextLine();
-				if(!temp.equals(""))		// It kept reading an empty line for some reason. That was really strange and gave me 
-											// a lot of trouble. If anyone has any information on this, I'd be very interested to know. 
-					versions.add(new Version(temp));
-			}
-			i.close();
-			// Bubble sort, I know. Please forgive me.
-			boolean done = false;
-			while (!done) {
-				done = true;
-				for (int q = 1; q < versions.size(); q++) {
-					if(switchable(versions.get(q-1),versions.get(q)))
-					{
-						Collections.swap(versions, q, q-1);
-						done = false;
-					}
-				}
-			}
-			for(Version v : versions)
-				System.out.println(v.orig);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
+        File f = new File("input.txt");
+        try {
+            Scanner i = new Scanner(f);
+            i.nextInt();		// I didn't really need to use the number, but I needed to get past it. 
+            ArrayList<Version> versions = new ArrayList<>();
+            String temp;
+            while (i.hasNext()) {
+                temp = i.nextLine();
+                if (!temp.equals("")) // It kept reading an empty line for some reason. That was really strange and gave me 
+                // a lot of trouble. If anyone has any information on this, I'd be very interested to know. 
+                {
+                    versions.add(new Version(temp));
+                }
+            }
+            i.close();
+            // Bubble sort, I know. Please forgive me.
+            boolean done = false;
+            while (!done) {
+                done = true;
+                for (int q = 1; q < versions.size(); q++) {
+                    if (switchable(versions.get(q - 1), versions.get(q))) {
+                        Collections.swap(versions, q, q - 1);
+                        done = false;
+                    }
+                }
+            }
+            for (Version v : versions) {
+                System.out.println(v.orig);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public static boolean switchable(Version earlier, Version later) {
-		if(earlier.major>later.major)
-			return true;
-		if(earlier.minor>later.minor && earlier.major == later.major)
-			return true;
-		if (earlier.patch>later.patch && earlier.major == later.major && earlier.minor == later.minor)
-			return true;
-		if (earlier.patch == later.patch && earlier.minor == later.minor && earlier.major == later.major)
-			return later.label != null;
-		return false;
-	}
+    public static boolean switchable(Version earlier, Version later) {
+        if (earlier.major > later.major) {
+            return true;
+        }
+        if (earlier.minor > later.minor && earlier.major == later.major) {
+            return true;
+        }
+        if (earlier.patch > later.patch && earlier.major == later.major && earlier.minor == later.minor) {
+            return true;
+        }
+        if (earlier.patch == later.patch && earlier.minor == later.minor && earlier.major == later.major) {
+            return later.label != null;
+        }
+        return false;
+    }
 
 }
