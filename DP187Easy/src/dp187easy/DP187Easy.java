dfa8e6f3323fc1d8450427a93b1837b47eeb90eb
ddsnowboard <ddsnowboard@gmail.com>
2014-11-03 19:37:39 -0600

    Comments and small change.
    I commented a little so I know what I did, and there was a line that could be a little more specific and robust, so I made it so.

--- DP187Easy/src/dp187easy/DP187Easy.java
+++ DP187Easy/src/dp187easy/DP187Easy.java
@@ -14,20 +14,30 @@ public class DP187Easy {
             Scanner s = new Scanner(f);
             int n = s.nextInt();
             HashMap map = new HashMap(n);
-            ArrayList<String> long_names = new ArrayList<>();
             String[] current;
+            // Makes the list of available commands. 
             for (int i = 0; i < n; i++) {
                 current = s.next().split(":");
                 map.put(current[0], current[1]);
                 long_names.add(current[1]);
             }
+            // Skips the rest of the line, whatever newline characters et 
+            // alia are on there. 
             s.nextLine();
             current = s.nextLine().split(" ");
+            // If it's a short flag with more than one character, I use this to
+            // be able to iterate through them. 
             ArrayList<Character> chars = new ArrayList<>();
             for (String t : current) {
+                // If the first two characters are dashes, it's a long flag; 
+                // just print it. 
                 if (t.charAt(0) == '-' && t.charAt(1) == '-') {
                     System.out.printf("flag: %s%n", t.substring(2));
+                // If the first character is a dash and it's longer than 2, then it's 
+                // a multi-short flag; look for it's long brothers and print them. 
                 } else if (t.charAt(0) == '-' && t.length() > 2) {
+                    // A stronger man would use an array, but I had no such luck. 
+                    // An ArrayList it is. 
                     chars.clear();
                     for (int p = 1; p < t.length(); p++) {
                         chars.add(t.charAt(p));
@@ -41,7 +51,9 @@ public class DP187Easy {
                             return;
                         }
                     }
-                } else if (t.charAt(0) == '-') {
+                // If it starts with a dash and isn't longer than 2, than it's a single
+                // short flag. Find it's long brother and print it. 
+                } else if (t.charAt(0) == '-' && t.length() == 2) {
                     if (map.get(t.substring(1)) != null) {
                             System.out.printf("flag: %s%n", map.get(t.substring(1)));
                         } else {
