51da01e98c31c5d568d5275471f9810b1fcc84ad
ddsnowboard <ddsnowboard@gmail.com>
2014-11-04 19:40:28 -0600

    Cleaned up the code in DP187Easy
    It was really bad, with one-letter variable names and all sorts of
    horribleness, but I did the refactoring thing, and now it's a lot
    better. I also got rid of the massive amounts of really gross nested
    functions and stuff, so that's good.

--- DP187Easy/src/dp187easy/DP187Easy.java
+++ DP187Easy/src/dp187easy/DP187Easy.java
@@ -3,7 +3,6 @@ package dp187easy;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -15,13 +14,16 @@ public class DP187Easy {
         try {
             File f = new File("input.txt");
             Scanner s = new Scanner(f);
-            int n = s.nextInt();
+            int number = s.nextInt();
             boolean expecting_argument = false;
             FlagGroup expecting_flag = null;
-            HashMap<String, FlagGroup> map = new HashMap<>(n);
+            HashMap<String, FlagGroup> map = new HashMap<>(number);
             String[] current;
+            // This is whatever we're looking at, stripped of dashes, turned into
+            // a string, whatever else needs to be done to make it easier to use. 
+            String working_copy;
             // Makes the list of available commands. 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < number; i++) {
                 current = s.next().split(":");
                 map.put(current[0].replace("*", ""), new FlagGroup(current[1], current[0].contains("*")));
             }
@@ -32,25 +34,27 @@ public class DP187Easy {
             // If it's a short flag with more than one character, I use this to
             // be able to iterate through them. 
             ArrayList<Character> chars = new ArrayList<>();
-            for (String t : current) {
+            for (String current_flag : current) {
                 // If the first two characters are dashes, it's a long flag; 
                 // just print it. 
-                if (t.charAt(0) == '-' && t.charAt(1) == '-') {
-                    if (t.contains("=")) {
-                        System.out.printf("flag: %s (value: %s)%n", t.split("=")[0].substring(2), t.split("=")[1]);
+                if (current_flag.substring(0, 2).equals("--")) {
+                    working_copy = current_flag.substring(2);
+                    if (working_copy.contains("=")) {
+                        System.out.printf("flag: %s (value: %s)%n", working_copy.split("=")[0], working_copy.split("=")[1]);
                     } else {
-                        System.out.printf("flag: %s%n", t.substring(2));
+                        System.out.printf("flag: %s%n", working_copy);
                     }
-                    // If the first character is a dash and it's longer than 2, then it's 
-                    // a multi-short flag; look for it's long brothers and print them. 
-                } else if (t.charAt(0) == '-' && t.length() > 2) {
-                    for (int p = 1; p < t.length(); p++) {
-                        if (map.get(String.valueOf(t.charAt(p))) != null) {
-                            if (!(p == t.length() - 1 && map.get(String.valueOf(t.charAt(p))).argument)) {
-                                System.out.printf("flag: %s%n", map.get(String.valueOf(t.charAt(p))).long_name);
+                // If the first character is a dash and it's longer than 2, then it's 
+                // a multi-short flag; look for it's long brothers and print them. 
+                } else if (current_flag.charAt(0) == '-' && current_flag.length() > 2) {
+                    for (int p = 1; p < current_flag.length(); p++) {
+                        working_copy = String.valueOf(current_flag.charAt(p));
+                        if (map.get(working_copy) != null) {
+                            if (!(p == current_flag.length() - 1 && map.get(working_copy).argument)) {
+                                System.out.printf("flag: %s%n", map.get(working_copy).long_name);
                             } else {
                                 expecting_argument = true;
-                                expecting_flag = map.get(String.valueOf(t.charAt(p)));
+                                expecting_flag = map.get(working_copy);
                             }
                         } else {
                             System.out.println("Error: You gave an argument that's not defined!");
@@ -59,25 +63,25 @@ public class DP187Easy {
                     }
                     // If it starts with a dash and isn't longer than 2, than it's a single
                     // short flag. Find it's long brother and print it. 
-                } else if (t.charAt(0) == '-' && t.length() == 2) {
-                    if (map.get(t.substring(1)) != null) {
-                        if (!(map.get(t.substring(1)).argument)) {
-                            System.out.printf("flag: %s%n", map.get(t.substring(1)).long_name);
+                } else if (current_flag.charAt(0) == '-' && current_flag.length() == 2) {
+                    working_copy = current_flag.replace("-", "");
+                    if (map.get(working_copy) != null) {
+                        if (!(map.get(working_copy).argument)) {
+                            System.out.printf("flag: %s%n", map.get(working_copy).long_name);
                         } else {
                             expecting_argument = true;
-                            expecting_flag = map.get(t.substring(1));
+                            expecting_flag = map.get(working_copy);
                         }
                     } else {
                         System.out.println("Error: You gave an argument that's not defined!");
-                        expecting_argument = false;
                         return;
                     }
                 } else {
                     if (expecting_argument) {
-                        System.out.printf("flag: %s (value: %s)%n", expecting_flag.long_name, t);
+                        System.out.printf("flag: %s (value: %s)%n", expecting_flag.long_name, current_flag);
                         expecting_argument = false;
                     } else {
-                        System.out.printf("parameter: %s%n", t);
+                        System.out.printf("parameter: %s%n", current_flag);
                     }
                 }
             }
