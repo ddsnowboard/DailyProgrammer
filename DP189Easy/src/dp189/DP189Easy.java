d9f86b851be68a97fbf2a695671a05549c2983ee
ddsnowboard <ddsnowboard@gmail.com>
2014-11-18 12:47:39 -0600

    Press on with DP189Easy

--- DP189Easy/src/dp189/DP189Easy.java
+++ DP189Easy/src/dp189/DP189Easy.java
@@ -5,8 +5,11 @@
  */
 package dp189;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -16,33 +19,18 @@ public class DP189Easy {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException if it can't find the words file. 
      */
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String difficulty;
-        ArrayList<String> easy = new ArrayList<>();
-        ArrayList<String> medium = new ArrayList<>();
-        ArrayList<String> hard = new ArrayList<>();
-        File f = new File("words.txt");
-        Scanner s = new Scanner(f);
-        String next;
-        while(s.hasNext())
+    public static void main(String[] args) throws FileNotFoundException {
+        String word = getWord();
+        boolean finished = false;
+        ArrayList<String> goodLetters = new ArrayList<>(word.length());
+        int misses = 0;
+        while (!finished)
         {
-            next = s.nextLine();
-            if(next.isEmpty())
-            {}
-            else if(next.length()<=5)
-                
+            System.out.println(getMan(misses));
+            System.out.println(getLetters(word, goodLetters));
         }
-        String word;
-        while (true) {
-            System.out.println("Do you want to play an EASY, MEDIUM, or HARD game?");
-            difficulty = input.nextLine();
-            if (difficulty.toLowerCase().equals("easy")) {
-                
-            }
-        }
-
     }
 
     public static String getMan(int i) {
@@ -117,4 +105,64 @@ public class DP189Easy {
         return man[i];
     }
 
-}
+    public static String getWord() throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        String difficulty;
+        ArrayList<String> easy = new ArrayList<>(15000);
+        ArrayList<String> medium = new ArrayList<>(30000);
+        ArrayList<String> hard = new ArrayList<>(75000);
+        File f = new File("words.txt");
+        Scanner s = new Scanner(f);
+        String next;
+        while (s.hasNext()) {
+            next = s.nextLine();
+            if (next.isEmpty()) {
+            } else if (next.length() <= 5) {
+                easy.add(next);
+            } else if (next.length() <= 7) {
+                medium.add(next);
+            } else {
+                hard.add(next);
+            }
+
+        }
+        String word;
+        Random random = new Random();
+        OUTER:
+        while (true) {
+            System.out.println("Do you want to play an EASY, MEDIUM, or HARD game?");
+            difficulty = input.nextLine();
+            switch (difficulty.toLowerCase()) {
+                case "easy":
+                    word = easy.get(random.nextInt(easy.size()));
+                    break OUTER;
+                case "medium":
+                    word = medium.get(random.nextInt(medium.size()));
+                    break OUTER;
+                case "hard":
+                    word = hard.get(random.nextInt(hard.size()));
+                    break OUTER;
+                default:
+                    System.out.println("That wasn't an available option");
+                    break;
+            }
+        }
+        s.close();
+        return word;
+    }
+    
+    public static String getLetters(String word, ArrayList<String> letters)
+    {
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i<word.length(); i++)
+        {
+            if(letters.contains(String.valueOf(word.charAt(i))))
+            {
+                output.append(String.valueOf(word.charAt(i)));
+            }
+            else
+                output.append("_");
+        }
+        return output.toString();
+    }
+    }
