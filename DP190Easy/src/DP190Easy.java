e03bab55ef8ac30752bf2691740c672e1949fb53
ddsnowboard <ddsnowboard@gmail.com>
2014-11-26 11:56:11 -0600

    Press on with DP190Easy
    I'm almost done, I think. I just need to debug and make the print statement
    exactly right. Unfortunately, the internet is running out and I am
    getting carsick, so it'll have to be later.

--- DP190Easy/src/DP190Easy.java
+++ DP190Easy/src/DP190Easy.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import static org.jsoup.Jsoup.connect;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,9 +21,33 @@ public class DP190Easy {
      */
     public static void main(String[] args) throws IOException {
         String url = "https://plus.googleapis.com/u/0/_/widget/render/comments?first_party_property=YOUTUBE&href=https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-        Document document = Jsoup.connect(url).get();
-        System.out.println(document.title());
-        Elements comments = 
+        String[] happyWords = {"love", "loved", "like", "liked", "awesome", "amazing", "good", "great", "excellent"};
+        String[] sadWords = {"hate", "hated", "dislike", "disliked", "awful", "terrible", "bad", "painful", "worst"};
+        int happyNum = 0;
+        int sadNum = 0;
+        Document document = connect(url).get();
+        Elements comments = document.getElementsByClass("Ct");
+        for (Element c : comments) {
+            for (String s : happyWords) {
+                happyNum += count(c.text(), s);
+            }
+            for (String s : sadWords)
+            {
+                sadNum += count(c.text(), s);
+            }
+        }
+        System.out.printf("There were %d happy words and %d sad words", happyNum, sadNum);
     }
-    
+
+    public static int count(String s, String search) {
+        int count = 0;
+        int len = search.length();
+        for (int i = 0; i < s.length() - len; i++) {
+            if (s.substring(i, i + len).equals(search)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
