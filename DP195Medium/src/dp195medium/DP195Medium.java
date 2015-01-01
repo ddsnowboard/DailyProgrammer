6c2c0c1b1b24263f51446d314e6fe480787a103b
ddsnowboard <ddsnowboard@gmail.com>
2014-12-31 23:45:35 -0600

    Finished DP195Medium
    It's not exactly beautiful, and it's slow, but it works usually.

--- DP195Medium/src/dp195medium/DP195Medium.java
+++ DP195Medium/src/dp195medium/DP195Medium.java
@@ -8,6 +8,8 @@ package dp195medium;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,38 +20,36 @@ import java.util.regex.Pattern;
  */
 public class DP195Medium {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws FileNotFoundException {
         File inFile = new File("input.txt");
         Pattern parser = Pattern.compile("(?<number>[0-9]+)d(?<sides>[0-9]+)");
         Scanner s = new Scanner(inFile);
         String input = s.nextLine();
-        Matcher m = parser.matcher(input.split(" ")[0]);
-        m.matches();
-        Die[] target = new Die[Integer.parseInt(m.group("number"))];
+        boolean found = false;
+        Matcher matcher = parser.matcher(input.split(" ")[0]);
+        matcher.matches();
+        Die[] target = new Die[Integer.parseInt(matcher.group("number"))];
         for (int i = 0; i < target.length; i++) {
-            target[i] = new Die(Integer.parseInt(m.group("sides")));
+            target[i] = new Die(Integer.parseInt(matcher.group("sides")));
         }
-        m.reset(input.split(" ")[1]);
-        m.matches();
-        Die[] game = new Die[Integer.parseInt(m.group("number"))];
+        matcher.reset(input.split(" ")[1]);
+        matcher.matches();
+        Die[] game = new Die[Integer.parseInt(matcher.group("number"))];
         for (int i = 0; i < game.length; i++) {
-            game[i] = new Die(Integer.parseInt(m.group("sides")));
+            game[i] = new Die(Integer.parseInt(matcher.group("sides")));
         }
         int targetNumber = 0;
         for (Die d : target) {
             targetNumber += d.roll();
         }
-        System.out.println(targetNumber);
+        System.out.printf("%d, ", targetNumber);
         Integer[] numbers = new Integer[game.length];
         for (int i = 0; i < game.length; i++) {
             numbers[i] = game[i].roll();
+            System.out.printf("%d ", numbers[i]);
         }
-        Arrays.sort(numbers, new ReverseComparator());
-        System.out.println(Arrays.toString(numbers));
-        // Incoming grossness; beware
+        System.out.println();
+        Arrays.sort(numbers, Collections.reverseOrder());
         int curr;
         StringBuilder currString;
         for (int i = 0; i < Math.pow(2, numbers.length); i++) {
@@ -57,23 +57,27 @@ public class DP195Medium {
             currString = new StringBuilder();
             for (int j = 0; j < numbers.length; j++) {
                 if ((i & ((int) Math.pow(2, j))) > 0) {
-                    if (j != 0) {
-                        currString.append("+");
-                    }
+                    currString.append("+");
                     curr += numbers[j];
                 } else {
                     currString.append("-");
                     curr -= numbers[j];
                 }
                 currString.append(numbers[j]);
-
             }
-            // Figure out how absolute values factor into this. (HEYO! pun.)
-            if (curr == targetNumber || curr == -1*targetNumber) {
-                System.out.println("Found it!");
-                System.out.println(currString.toString());
+            if (curr == targetNumber) {
+                System.out.println(currString.toString().replaceAll("^\\+", ""));
+                found = true;
+                break;
+            } else if (curr == -1 * targetNumber) {
+//                You might not want to look at this line...
+                System.out.println(currString.toString().replace("-", "b").replace("+", "a").replace("b", "+").replace("a", "-").replaceAll("^\\+", ""));
+                found = true;
                 break;
             }
+        }
+        if (!found) {
+            System.out.println("I didn't find anything.");
         }
     }
 
