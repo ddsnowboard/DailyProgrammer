548e294406f4dd41bea86548fa4833b1c3b35000
Will Koster <ddsnowboard@gmail.com>
2016-05-13 23:09:57 -0500

    Inexplicable PHP problems
    I am beginnig to realize why this language is so widely maligned.

--- DP266Medium/main.php
+++ DP266Medium/main.php
@@ -3,35 +3,89 @@ class Node {
     private $connections = [];
     public function connect(Node $node)
     {
-        array_push($this->$connections, $node);
+        array_push($this->connections, $node);
     }
     public function getConnections()
     {
-        return $this->$connections;
+        return $this->connections;
     }
     public function distance(Node $node)
     {
         // Will this work? Who knows!
         $n = 0;
-        $nodes = $this->$connections;
-        while(!array_search($nodes, $node))
+        $nodes = $this->connections;
+        while(!array_search($node, $nodes))
         {
             $n++;
-            $nodes = array_map(array_merge, array_map(Node::getConnections, $nodes));
+            $nodes = array_map("array_merge", array_map(function ($no) use ($nodes)
+            {
+                var_dump($nodes);
+                echo "\n\n\n\n\n";
+                var_dump($no);
+                echo "\n\n\n\n\n";
+                echo "\n\n\n\n\n";
+                echo "\n\n\n\n\n";
+                echo "\n\n\n\n\n";
+                echo "\n\n\n\n\n";
+                echo "\n\n\n\n\n";
+                echo "\n\n\n\n\n";
+                echo "\n\n\n\n\n";
+                // Apparently, $no is an array here. I'm so confused
+                return $no->getConnections();
+            }, $nodes));
         }
         return $n;
     }
 }
 class Graph {
-    private $nodes = [];
+    private $nodes = array();
     public function eccentricity(Node $node)
     {
-        return max(array_map(function ($n) {
+        return max(array_map(function ($n) use ($node) {
             return $node->distance($n);
-        }, $this->$nodes));
+        }, $this->nodes));
     }
-    public addNode(Node $node)
+    public function radius()
     {
-        array_push($this->$nodes, $node);
+        return min(array_map(array($this, "eccentricity"), $this->nodes));
+    }
+    public function diameter()
+    {
+        return max(array_map(array($this, "eccentricity"), $this->nodes));
+    }
+    public function addNode($idx, Node $node)
+    {
+        if(is_int($idx)){
+            if(!isset($nodes[$i]))
+                $this->nodes[$i] =  $node;
+        }
+    }
+    public function &getNode($i)
+    {
+        if(is_int($i)){
+            if(!isset($this->nodes[$i]))
+            {
+                $this->nodes[$i] = new Node();
+            }
+            return $this->nodes[$i];
+        }
     }
 }
+
+$file = fopen("input.txt", "r");
+fgets($file);
+$currString = "";
+$graph = new Graph();
+while(!feof($file))
+{
+    $currString = fgets($file);
+    if(!empty($currString))
+    {
+        $a = 0;
+        $b = 0;
+        sscanf($currString, "%d %d", $a, $b);
+        $graph->getNode($a)->connect($graph->getNode($b));
+        $graph->getNode($b)->connect($graph->getNode($a));
+    }
+}
+echo $graph->radius();
