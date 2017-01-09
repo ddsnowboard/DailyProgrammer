b7f34c6b8add08511367ff11ca659bbfe588a79f
Will Koster <jameswk2@illinois.edu>
2017-01-09 16:36:35 -0600

    It doesn't work
    It's getting close, but not quite. I don't know what's wrong with it
    though.

--- DP298Medium/src/main.rs
+++ DP298Medium/src/main.rs
@@ -7,39 +7,72 @@ fn main() {
     let mut f = File::open("input.txt").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
-    let lines = s.lines();
-    for line in lines {
-        let mut opens = 0;
+
+
+
+    for line in s.lines() {
         let mut line = String::from(line);
-        let mut badParen = None;
-        for c in line.char_indices() {
-            match c
+        let mut currIdx = 0;
+        let badIndex: usize;
+        while currIdx < line.len() {
+            let currChar = line.chars().nth(currIdx).unwrap();
+            if  currChar == ')'
             {
-                (_, '(') => opens += 1,
-                (x, ')') => {
-                    if opens == 0 {
-                        badParen = Some(x);
-                        break;
-                    }
-                    else 
-                    {
-                        opens -= 1;
-                    }
+                badIndex = currIdx;
+                surroundIndex(&mut line, badIndex, "**");
+                break;
+            }
+            else if currChar == '('
+            {
+                match find_match(&mut line, currIdx)
+                {
+                    Ok(x) => currIdx = x,
+                    Err(x) => {badIndex = x; surroundIndex(&mut line, badIndex, "**"); break;}
                 }
-                (_, _) => {}
             }
         }
-        match badParen {
-            Some(parenLocation) => 
-            {
-                line.insert(parenLocation, '*');
-                line.insert(parenLocation, '*');
-                // I need to move 3 forward to get the two asterisks that I just inserted and the paren
-                line.insert(parenLocation + 3, '*');
-                line.insert(parenLocation + 3, '*');
-            }, 
-            None => {}
-        }
         println!("{}", line);
+    }
+}
+
+fn find_match(s: &String, startingIdx: usize) -> Result<usize, usize>
+{
+    let mut chars = s.chars();
+    let mut idx = startingIdx;
+    let retval: Result<_,_> = Err(0);
+    while idx < s.len()
+    {
+        match chars.nth(idx).unwrap()
+        {
+            '(' => {
+                match find_match(s, idx) {
+                    Ok(x) => {idx = x},
+                    Err(x) => return Err(x), 
+                }
+            }
+            ')' => return Ok(idx), 
+            _ => {}
+        }
+    }
+    retval
+}
+
+fn surroundIndex(s: &mut String, currIdx: usize, toInsert: &str)
+{
+    s.insert_string(currIdx, toInsert);
+    s.insert_string(currIdx + 1 + toInsert.len(), toInsert);
+}
+
+trait InsertsString {
+    fn insert_string(&mut self, usize, &str);
+}
+
+impl InsertsString for String {
+    fn insert_string(&mut self, idx: usize, toInsert: &str)
+    {
+        println!("What's going on? idx is {} and toInsert is {}", idx, toInsert);
+        for currIndex in (idx..toInsert.len()).rev() {
+            self.insert(idx, toInsert.chars().nth(currIndex).unwrap()); 
+        }
     }
 }
