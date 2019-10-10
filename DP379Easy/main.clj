b61e55d7331f4440f9ba76ebb22954b37b77eeb7
Will Koster <jameswk2@illinois.edu>
2019-10-09 20:48:51 -0500

    Start on DP379Easy
    Clojure is weird. I might get there eventually.

---
+++ DP379Easy/main.clj
@@ -0,0 +1,12 @@
+(defn deltifyBrackets [[fst snd & rest]]
+  (if (= snd nil)
+  )
+(def brackets ['(10000 0.0)
+                '(30000 0.1)
+                '(100000 0.25)
+                '(##Inf 0.4)])
+
+(defn tax [income] income)
+
+(println (tax 44))
+(println brackets)
